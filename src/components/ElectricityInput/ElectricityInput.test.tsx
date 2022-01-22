a3e0481e592050e7d41bd39af7d1b3b661a25ca0
Judith <72439973+judicodes@users.noreply.github.com>
2022-01-22 21:06:03 +0100

    Add Input validation

--- src/components/ElectricityInput/ElectricityInput.test.tsx
+++ src/components/ElectricityInput/ElectricityInput.test.tsx
@@ -1,15 +1,37 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import ElectricityInput from "./ElectricityInput";
 
-beforeEach(() => {
-  render(<ElectricityInput />);
-});
-
 describe("Location Input", () => {
+  render(<ElectricityInput />);
   it("renders Location Input", () => {
     expect(
       screen.getByLabelText("Location (Country)", {
+        exact: false,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("shows no error initially", () => {
+    render(<ElectricityInput />);
+    expect(
+      screen.queryByText("Please select a location", {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
+  });
+
+  it("shows an error message when no country is selected after user interaction", async () => {
+    render(<ElectricityInput />);
+
+    const inputElement = screen.getByLabelText("Location (Country)", {
+      exact: false,
+    });
+    await userEvent.type(inputElement, "xyz");
+    await userEvent.click(document.body);
+    expect(
+      screen.getByText("Please select a location", {
         exact: false,
       })
     ).toBeInTheDocument();
@@ -18,8 +40,73 @@ describe("Location Input", () => {
 
 describe("Electricity Usage Input", () => {
   it("renders Electricity Usage Input", () => {
+    render(<ElectricityInput />);
     expect(
       screen.getByLabelText("Electricity Usage", {
+        exact: false,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("shows no error initially", async () => {
+    render(<ElectricityInput />);
+    expect(
+      screen.queryByText("Please enter a positive number", {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
+  });
+
+  it("shows an error on blur", async () => {
+    render(<ElectricityInput />);
+    await userEvent.click(
+      screen.getByLabelText("Electricity Usage", {
+        exact: false,
+      })
+    );
+    await userEvent.click(document.body);
+    expect(
+      screen.queryByText("Please enter a positive number", {
+        exact: false,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("shows no error when entering positive numbers", async () => {
+    render(<ElectricityInput />);
+    const electricityUsageInput = screen.getByLabelText("Electricity Usage", {
+      exact: false,
+    });
+    await userEvent.type(electricityUsageInput, "35.789");
+
+    expect(
+      screen.queryByText("Please enter a positive number", {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
+  });
+
+  it("should show an error when entering a non-numerical value", async () => {
+    render(<ElectricityInput />);
+    const electricityUsageInput = screen.getByLabelText("Electricity Usage", {
+      exact: false,
+    });
+    await userEvent.type(electricityUsageInput, "abc");
+    expect(
+      screen.queryByText("Please enter a positive number", {
+        exact: false,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("should show an error when entering negative numbers", async () => {
+    render(<ElectricityInput />);
+    const electricityUsageInput = screen.getByLabelText("Electricity Usage", {
+      exact: false,
+    });
+    await userEvent.type(electricityUsageInput, "-5");
+    expect(
+      screen.queryByText("Please enter a positive number", {
         exact: false,
       })
     ).toBeInTheDocument();
@@ -28,6 +115,7 @@ describe("Electricity Usage Input", () => {
 
 describe("Unit Selection", () => {
   it("renders Unit select", () => {
+    render(<ElectricityInput />);
     expect(
       screen.getByLabelText("Unit", {
         exact: false,
--- src/components/ElectricityInput/ElectricityInput.tsx
+++ src/components/ElectricityInput/ElectricityInput.tsx
@@ -1,80 +1,130 @@
 import {
   Autocomplete,
-  FormGroup,
-  Input,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { countries } from "../../assets/countries";
 import "./ElectricityInput.css";
 
 function ElectricityInput() {
+  const units = ["kwh", "mwh"];
+
   const [selectedCountry, setSelectedCountry] = React.useState<string | null>(
     null
   );
-  const [electricityUsage, setElectricityUsage] = React.useState(0);
+  const [electricityUsage, setElectricityUsage] = React.useState<number | null>(
+    null
+  );
+  const [electricityUsageInput, setElectricityUsageInput] = React.useState("");
   const [selectedUnit, setSelectedUnit] = React.useState("kwh");
 
-  const units = ["kwh", "mwh"];
+  const [hasElectricityUsageInputError, setHasElectricityUsageInputError] =
+    React.useState(false);
+  const [hasCountrySelectError, setHasCountrySelectError] =
+    React.useState(false);
+
+  const validateCountrySelect = () => {
+    if (selectedCountry === null || selectedCountry.trim() === "") {
+      setHasCountrySelectError(true);
+    } else {
+      setHasCountrySelectError(false);
+    }
+  };
+
+  const validateElectricityUsageInput = (value: string) => {
+    const postiveNumberRegExp = /^[+]?([0-9]+(?:[.][0-9]*)?|\.[0-9]+)$/;
+    if (!postiveNumberRegExp.test(value)) {
+      setHasElectricityUsageInputError(true);
+    } else {
+      setHasElectricityUsageInputError(false);
+    }
+  };
+
+  const handleElectricityInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    validateElectricityUsageInput(event.target.value);
+    setElectricityUsageInput(event.target.value);
+  };
+
+  useEffect(() => {
+    setElectricityUsage(
+      hasElectricityUsageInputError ? null : parseFloat(electricityUsageInput)
+    );
+  }, [electricityUsageInput]);
 
   return (
     <div className="electricity-input">
-      <FormGroup className="form-group">
-        <InputLabel htmlFor="country-select" required>
-          Location (Country)
-        </InputLabel>
-        <Autocomplete
-          id="country-select"
-          data-testid="location-input"
-          disablePortal
-          options={countries}
-          getOptionLabel={(option) => option.displayName}
-          onChange={(_, newValue) => {
-            setSelectedCountry(newValue?.code ?? null);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-          className="mb-2"
-        />
+      <InputLabel htmlFor="country-select" required>
+        Location (Country)
+      </InputLabel>
+      <Autocomplete
+        id="country-select"
+        data-testid="location-input"
+        disablePortal
+        options={countries}
+        getOptionLabel={(option) => option.displayName}
+        onChange={(_, newValue) => {
+          setSelectedCountry(newValue?.code ?? null);
+        }}
+        onBlur={validateCountrySelect}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            error={hasCountrySelectError}
+            helperText={hasCountrySelectError ? "Please select a location" : ""}
+            onChange={() => {
+              setHasCountrySelectError(false);
+            }}
+          />
+        )}
+        className="mb-2"
+      />
 
-        <div className="row">
-          <div className="column electricity-input">
-            <InputLabel
-              htmlFor="electricity-usage-input"
-              className="align-start"
-              required
-            >
-              Electricity Usage
-            </InputLabel>
-            <Input
-              id="electricity-usage-input"
-              type="number"
-              value={electricityUsage}
-              onChange={(event) =>
-                setElectricityUsage(parseFloat(event.target.value))
-              }
-            />
-          </div>
-          <div className="column unit">
-            <InputLabel id="unit-select-label" className="align-start">
-              Unit
-            </InputLabel>
-            <Select
-              labelId="unit-select-label"
-              value={selectedUnit}
-              onChange={(event) => setSelectedUnit(event.target.value)}
-            >
-              {units.map((unit) => (
-                <MenuItem key={unit} value={unit}>
-                  {unit}
-                </MenuItem>
-              ))}
-            </Select>
-          </div>
+      <div className="row">
+        <div className="column electricity-input">
+          <InputLabel
+            htmlFor="electricity-usage-input"
+            className="align-start"
+            required
+          >
+            Electricity Usage
+          </InputLabel>
+          <TextField
+            id="electricity-usage-input"
+            value={electricityUsageInput}
+            onChange={handleElectricityInputChange}
+            onBlur={(event) =>
+              validateElectricityUsageInput(event.target.value)
+            }
+            error={hasElectricityUsageInputError}
+            helperText={
+              hasElectricityUsageInputError
+                ? "Please enter a positive number"
+                : ""
+            }
+          />
         </div>
-      </FormGroup>
+        <div className="column unit">
+          <InputLabel id="unit-select-label" className="align-start">
+            Unit
+          </InputLabel>
+          <Select
+            labelId="unit-select-label"
+            value={selectedUnit}
+            onChange={(event) => setSelectedUnit(event.target.value)}
+          >
+            {units.map((unit) => (
+              <MenuItem key={unit} value={unit}>
+                {unit}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+      </div>
     </div>
   );
 }
